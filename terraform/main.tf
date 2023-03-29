d9b1be90a9014df7b7829fad8d30154531f7f687
Qu <juntq@a483e76f9206.ant.amazon.com>
2023-03-29 00:07:45 -0700

    create account test-aft-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "test-aft-1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "juntq+test_aft_1@amazon.com"
+    AccountName               = "test-aft-1"
+    ManagedOrganizationalUnit = "test"
+    SSOUserEmail              = "juntq+test_aft_1@amazon.com"
+    SSOUserFirstName          = "testaft1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "test-aft-1"
+}
